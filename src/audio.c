0790852055c416ec64637a8e90cd7ff32273a56d
yngtdd <todd@elitehrv.com>
2021-09-25 23:25:20 -0400

    Start template for audio files

---
+++ src/audio.c
@@ -0,0 +1,14 @@
+#include "raylib.h"
+
+/* Play simple 8-bit music
+ */
+void play_music()
+{
+	InitAudioDevice();
+
+	Music music = LoadMusicStream("../assets/audio.xm");
+  music.looping = false;
+	float pitch = 1.0f;
+
+	PlayMusicStream(music);
+}
---
+++ src/audio.h
@@ -0,0 +1,6 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+void play_music();
+
+#endif // Audio implementation
