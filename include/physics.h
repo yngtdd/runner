237065fd1a7be15952534f84d0b836f7ab539b70
yngtdd <todd@elitehrv.com>
2021-10-04 18:42:34 -0400

    Add physics bodies

--- include/physics.h
+++ include/physics.h
@@ -6,4 +6,10 @@
 
 PhysicsBody create_floor(int screen_width, int screen_height);
 
+PhysicsBody create_platform(int screen_width, int screen_height, float x_offset, float y_offset);
+
+PhysicsBody create_wall(int screen_width, int screen_height, int offset);
+
+PhysicsBody create_body(int screen_width, int screen_height);
+
 #endif // Physics implementation
--- src/runner.c
+++ src/runner.c
@@ -8,6 +8,8 @@
 
 #define SCREEN_WIDTH (800)
 #define SCREEN_HEIGHT (450)
+#define VELOCITY    0.5f
+
 
 int main(void)
 {
@@ -19,20 +21,48 @@ int main(void)
     Music music = load_music();
     PlayMusicStream(music);
 
-    Texture2D texture = LoadTexture(ASSETS_PATH "dino.png");
+    PhysicsBody floor = create_floor(SCREEN_WIDTH, SCREEN_HEIGHT);
+    PhysicsBody platform_left = create_platform(SCREEN_WIDTH, SCREEN_HEIGHT, 0.25f, 0.6f);
+    PhysicsBody platfrom_right = create_platform(SCREEN_WIDTH, SCREEN_HEIGHT, 0.75f, 0.6f);
+    PhysicsBody wall_right = create_wall(SCREEN_WIDTH, SCREEN_HEIGHT, 5);
+    PhysicsBody player = create_body(SCREEN_WIDTH, SCREEN_HEIGHT);
+
 
     while (!WindowShouldClose())
     {
         UpdateMusicStream(music);
+        UpdatePhysics();
+
+        if (IsKeyDown(KEY_RIGHT)) player->velocity.x = VELOCITY;
+        else if (IsKeyDown(KEY_LEFT)) player->velocity.x = -VELOCITY;
+
+        if (IsKeyDown(KEY_UP) && player->isGrounded) player->velocity.y = -VELOCITY*4;
+
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
+        DrawFPS(SCREEN_WIDTH - 90, SCREEN_HEIGHT - 30);
+
+        int bodies_count = GetPhysicsBodiesCount();
+        for (int i = 0; i < bodies_count; i++)
+        {
+            PhysicsBody body = GetPhysicsBody(i);
+            int vertex_count = GetPhysicsShapeVerticesCount(i);
+            for (int j = 0; j < vertex_count; j++)
+            {
+                Vector2 vertex_a = GetPhysicsShapeVertex(body, j);
+                int jj = (((j + 1) < vertex_count) ? (j + 1) : 0);
+                Vector2 vertex_b = GetPhysicsShapeVertex(body, jj);
+                DrawLineV(vertex_a, vertex_b, GREEN);
+            }
+        }
 
         EndDrawing();
     }
 
     UnloadMusicStream(music);
     CloseAudioDevice();
+    ClosePhysics();
     CloseWindow();
 
     return 0;
