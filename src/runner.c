e7cb5a8ca439dd6e2faaeac0a6357044956f4d9b
yngtdd <todd@elitehrv.com>
2021-10-04 23:29:56 -0400

    Fix tab spacing

--- src/runner.c
+++ src/runner.c
@@ -29,31 +29,31 @@ int main(void)
     while (!WindowShouldClose())
     {
         UpdateMusicStream(music);
-				//UpdatePhysics();
+        //UpdatePhysics();
 
-				if (IsKeyDown(KEY_RIGHT)) player->velocity.x = VELOCITY;
-				else if (IsKeyDown(KEY_LEFT)) player->velocity.x = -VELOCITY;
+        if (IsKeyDown(KEY_RIGHT)) player->velocity.x = VELOCITY;
+        else if (IsKeyDown(KEY_LEFT)) player->velocity.x = -VELOCITY;
 
-				if (IsKeyDown(KEY_UP) && player->isGrounded) player->velocity.y = -VELOCITY*4;
+        if (IsKeyDown(KEY_UP) && player->isGrounded) player->velocity.y = -VELOCITY*4;
 
         BeginDrawing();
 
         ClearBackground(BLACK);
-				DrawFPS(SCREEN_WIDTH - 90, SCREEN_HEIGHT - 30);
+        DrawFPS(SCREEN_WIDTH - 90, SCREEN_HEIGHT - 30);
 
-				int bodies_count = GetPhysicsBodiesCount();
-				for (int i = 0; i < bodies_count; i++)
-				{
-						PhysicsBody body = GetPhysicsBody(i);
-						int vertex_count = GetPhysicsShapeVerticesCount(i);
-						for (int j = 0; j < vertex_count; j++)
-						{
-								Vector2 vertex_a = GetPhysicsShapeVertex(body, j);
-								int jj = (((j + 1) < vertex_count) ? (j + 1) : 0);
-								Vector2 vertex_b = GetPhysicsShapeVertex(body, jj);
-								DrawLineV(vertex_a, vertex_b, GREEN);
-						}
-				}
+        int bodies_count = GetPhysicsBodiesCount();
+        for (int i = 0; i < bodies_count; i++)
+        {
+            PhysicsBody body = GetPhysicsBody(i);
+            int vertex_count = GetPhysicsShapeVerticesCount(i);
+            for (int j = 0; j < vertex_count; j++)
+            {
+                Vector2 vertex_a = GetPhysicsShapeVertex(body, j);
+                int jj = (((j + 1) < vertex_count) ? (j + 1) : 0);
+                Vector2 vertex_b = GetPhysicsShapeVertex(body, jj);
+                DrawLineV(vertex_a, vertex_b, GREEN);
+            }
+        }
 
         EndDrawing();
     }
