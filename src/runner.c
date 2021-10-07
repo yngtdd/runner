af83f399f77621fde12bf2c61a6de2dab52bdaa7
yngtdd <todd@elitehrv.com>
2021-10-07 01:02:59 -0400

    Fix spacing

--- src/runner.c
+++ src/runner.c
@@ -31,14 +31,14 @@ int main(void)
     PhysicsBody wall_right = create_wall(SCREEN_WIDTH, SCREEN_HEIGHT, 7);
     PhysicsBody player = create_body(SCREEN_WIDTH/2, SCREEN_HEIGHT);
 
-		bool pause = 0;
-		int frames_counter = 0;
+    bool pause = 0;
+    int frames_counter = 0;
 
     while (!WindowShouldClose())
     {
-				if (IsKeyPressed(KEY_SPACE)) pause = !pause;
+        if (IsKeyPressed(KEY_SPACE)) pause = !pause;
 
-				DrawTexture(bomberman, 0, 0, RAYWHITE);
+        DrawTexture(bomberman, 0, 0, RAYWHITE);
 
         if (!pause)
         {
@@ -48,21 +48,21 @@ int main(void)
             if (IsKeyDown(KEY_RIGHT)) player->velocity.x = VELOCITY;
             else if (IsKeyDown(KEY_LEFT)) player->velocity.x = -VELOCITY;
 
-				    if (IsKeyDown(KEY_UP) && player->isGrounded) player->velocity.y = -VELOCITY*4;
+            if (IsKeyDown(KEY_UP) && player->isGrounded) player->velocity.y = -VELOCITY*4;
 
-				    int bodies_count = GetPhysicsBodiesCount();
-				    for (int i = 0; i < bodies_count; i++)
-				    {
-						    PhysicsBody body = GetPhysicsBody(i);
-						    int vertex_count = GetPhysicsShapeVerticesCount(i);
-						    for (int j = 0; j < vertex_count; j++)
-						    {
-								    Vector2 vertex_a = GetPhysicsShapeVertex(body, j);
-								    int jj = (((j + 1) < vertex_count) ? (j + 1) : 0);
-								    Vector2 vertex_b = GetPhysicsShapeVertex(body, jj);
-								    DrawLineV(vertex_a, vertex_b, GREEN);
-                }
-             }
+            int bodies_count = GetPhysicsBodiesCount();
+            for (int i = 0; i < bodies_count; i++)
+            {
+                    PhysicsBody body = GetPhysicsBody(i);
+                    int vertex_count = GetPhysicsShapeVerticesCount(i);
+                    for (int j = 0; j < vertex_count; j++)
+                    {
+                            Vector2 vertex_a = GetPhysicsShapeVertex(body, j);
+                            int jj = (((j + 1) < vertex_count) ? (j + 1) : 0);
+                            Vector2 vertex_b = GetPhysicsShapeVertex(body, jj);
+                            DrawLineV(vertex_a, vertex_b, GREEN);
+                    }
+            }
         }
         else frames_counter++;
 
